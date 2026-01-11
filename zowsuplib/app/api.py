c1f163b17f324c47a435bb07c7169a566dac911b
weslenpy <weslenjhony@gmail.com>
2026-01-11 01:40:29 -0300

    fix to warning level

--- zowsuplib/app/api.py
+++ zowsuplib/app/api.py
@@ -1139,7 +1139,7 @@ class ZowsupClient:
         env: Optional[str] = None,
         proxy: Optional[str] = None,
         auto_connect: bool = False,
-        log_level: Optional[str] = "DEBUG",
+        log_level: Optional[str] = "WARNING",
     ) -> None:
         """
         Cria um novo cliente de alto nível completamente isolado.
